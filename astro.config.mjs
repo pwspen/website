ffcb6f2cd1931824a1cc62291c515123753a0fb3
pwspen <110143357+pwspen@users.noreply.github.com>
2024-12-29 00:26:25 -0500

    new site!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,34 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://synapsomorphy.com",
-  //   base: "/website",
-  integrations: [mdx(), sitemap(), react(), tailwind()],
-  vite: {
-    assetsInclude: ["**/*.json"],
+  site: 'https://synapsomorphy.com',
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      // Alternatively, provide multiple themes
+      // See note below for using dual light/dark themes
+      themes: {
+        light: 'poimandres',
+        dark: 'catppuccin-latte'
+      }
+    }
   },
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@layouts': '/src/layouts',
+        '@utils': '/src/utils',
+        '@consts': '/src/consts'
+      }
+    }
+  }
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Patrick Spencer";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'patrick\'s site'
+export const SITE_DESCRIPTION = 'Welcome to my website!'
