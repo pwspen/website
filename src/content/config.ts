ef4827560d1a6b95fe8aa85417b2ac7b65e6817c
pwspen <110143357+pwspen@users.noreply.github.com>
2024-12-29 18:03:51 -0500

    actually fixed using schema?

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,15 @@
 import { defineCollection, z } from 'astro:content'
-import { Image } from 'astro:assets'
 
 const blog = defineCollection({
     type: 'content',
     // Type-check frontmatter using a schema
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
         // Transform string to Date object
         pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
-        heroImage: z.string().optional(),
+        heroImage: image().optional(),
         tags: z.array(z.string()).optional(),
     }),
 })
