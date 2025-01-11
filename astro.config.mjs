fe663d04ef86a010e113cee7caa21f08e9bfb55a
pwspen <110143357+pwspen@users.noreply.github.com>
2025-01-11 17:13:55 -0500

    swap redirect method

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,5 +35,8 @@ export default defineConfig({
       }
     },
     remarkPlugins: [remarkReadingTime]
+  },
+  redirects: {
+    '/portfolio/': '/'
   }
 });
--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-// src/middleware.ts
-import { defineMiddleware } from 'astro:middleware';
-
-export const onRequest = defineMiddleware((context, next) => {
-  if (context.url.pathname === '/portfolio/') {
-    return context.redirect('/');
-  }
-  return next();
-});
